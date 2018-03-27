89e0d25b6e994dee6f257f87ca3b6b5cb44058e4
Jan ten Haaf <janth96@hotmail.com>
2018-03-27 17:04:49 +0200

    Homepage

--- resources/views/auth/account.blade.php
+++ resources/views/auth/account.blade.php
@@ -1,17 +1,18 @@
 @extends('index')
 
 @section('content')
-
-    <div class="account">
-        <div class="personal_information card">
-            <h1>Persoonlijke gegevens</h1>
-            <ul>
-                <li>Naam: {{ $user->name }}</li>
-                <li>E-mail: {{ $user->email }}</li>
-            </ul>
-        </div>
-        <div class="orders card">
-            <h1>Bestellingen</h1>
+    <div class="row">
+        <div class="account">
+            <div class="personal_information card">
+                <h1>Persoonlijke gegevens</h1>
+                <ul>
+                    <li>Naam: {{ $user->name }}</li>
+                    <li>E-mail: {{ $user->email }}</li>
+                </ul>
+            </div>
+            <div class="orders card">
+                <h1>Bestellingen</h1>
+            </div>
         </div>
     </div>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,16 @@
 
 @section('content')
 
-    <div class="row">
-        <h1>Hello world</h1>
+    <div class="home">
+        <div class="home_banner">
+            <img src="{{ asset('/img/onkruid-banner.jpg') }}">
+            <div class="row">
+                <h1>Onkruid voor iedere tuin!</h1>
+            </div>
+        </div>
+        <div class="row">
+            <p>Bekijk onze nieuwe collectie!</p>
+            <a class="btn" href="/producten">Producten</a>
+        </div>
     </div>
-
 @endsection
